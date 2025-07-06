018d51ac8970a09b6b2e42b47c8ad7f1538822b5
saisumith321 <79842398+saisumith321@users.noreply.github.com>
2025-07-06 17:52:02 +0530

    Update drop_down_select-and_all_topics pratice.py

--- drop_down_select-and_all_topics pratice.py
+++ drop_down_select-and_all_topics pratice.py
@@ -32,20 +32,18 @@ driver.maximize_window()
 # time.sleep(5)
 
 
-#date picker
+DATE PICKER
 
 # driver.find_element(By.CLASS_NAME,"hasDatepicker").click()
-#
-#
+
 # month = "December"
 # year = "2025"
 # date = "13"
-#
+
 # while True:
 #     mon = driver.find_element(By.XPATH,"//span[@class='ui-datepicker-month']").text
 #     ye = driver.find_element(By.XPATH,"//span[@class='ui-datepicker-year']").text
-#
-#
+
 #     if month == mon and year == ye:
 #         dat = driver.find_elements(By.XPATH, "//*[@id='ui-datepicker-div']/table/tbody/tr/td/a")
 #         for ele in dat:
@@ -57,7 +55,7 @@ driver.maximize_window()
 #         driver.find_element(By.XPATH,"//span[@class='ui-icon ui-icon-circle-triangle-e']").click()
 
 
-# click the chekbox
+CLICK THE CHECKBOX
 
 # click = driver.find_elements(By.XPATH,"//*[@id='post-body-1307673142697428135']/div[4]/div/label")
 # l = []
@@ -66,24 +64,24 @@ driver.maximize_window()
 # print(l)
 # print(len(l))
 
-#alters
+ALTERS IN WINDOWS
 
 # driver.find_element(By.ID,"alertBtn").click()
 # time.sleep(5)
 # alert = driver.switch_to.alert
 # alert.accept()
-#
+
 # driver.find_element(By.ID, "confirmBtn").click()
 # time.sleep(4)
 # alert = driver.switch_to.alert
 # alert.accept()
-#
+
 # driver.find_element(By.ID, "promptBtn").click()
 # time.sleep(4)
 # alert = driver.switch_to.alert
 # alert.send_keys("sai sumith")
 
-#GO BACK TO FIRST WINDOW AND CLICK GO TO FORWARD WINDOW
+GO BACK TO FIRST WINDOW AND CLICK GO TO FORWARD WINDOW
 # driver.find_element(By.LINK_TEXT,"Blog").click()
 # driver.back()
 # time.sleep(3)
@@ -99,39 +97,44 @@ driver.maximize_window()
 # time.sleep(3)
 # print("Switched back to main tab:", driver.title)
 
-#drag_and drop
+DRAG AND DROP
 
-source = driver.find_element(By.XPATH, "//*[@id='draggable']")
-target = driver.find_element(By.XPATH, "//*[@id='droppable']")
+# source = driver.find_element(By.XPATH, "//*[@id='draggable']")
+# target = driver.find_element(By.XPATH, "//*[@id='droppable']")
+# actions = ActionChains(driver)
+# actions.drag_and_drop(source, target).perform()
+# ele = driver.find_element(By.XPATH,"//*[@id='post-body-1307673142697428135']/div[8]/button")
+# actions = ActionChains(driver)
+# actions.move_to_element(ele).perform()
+# time.sleep(5)
 
-actions = ActionChains(driver)
-actions.drag_and_drop(source, target).perform()
+WEB SCRAPING WITH TABLE DATA
+# print("opening the browser")
+# driver.get("https://testautomationpractice.blogspot.com/")
+# driver.maximize_window()
+# f = driver.find_elements(By.XPATH, "//*[@id='productTable']/tbody/tr")
+# print(len(f))
+# print("finding the subject selenium with price")
+# Author = []
+# subject = []
+# for row in range(1,len(f) + 1):
+#     A = driver.find_element(By.XPATH, f"//*[@id='productTable']/tbody/tr{[row]}/td[2]")
+#     s = driver.find_element(By.XPATH,f"//*[@id='productTable']/tbody/tr{[row]}/td[3]")
+#     Author.append(A.text)
+#     subject.append(s.text)
+#     if s.text == "$5.99" or s.text == "$8.99":
+#         p = driver.find_element(By.XPATH,f"//*[@id='productTable']/tbody/tr{[row]}/td[4]/input")
+#         p.click()
+#         time.sleep(2)
+# print(Author)
 
-ele = driver.find_element(By.XPATH,"//*[@id='post-body-1307673142697428135']/div[8]/button")
 
-actions = ActionChains(driver)
-actions.move_to_element(ele).perform()
-time.sleep(5)
-
-#WEB SCRAPING WITH TABLE DATA
-print("opening the browser")
-driver.get("https://testautomationpractice.blogspot.com/")
-driver.maximize_window()
-f = driver.find_elements(By.XPATH, "//*[@id='productTable']/tbody/tr")
-print(len(f))
-print("finding the subject selenium with price")
-Author = []
-subject = []
-for row in range(1,len(f) + 1):
-    A = driver.find_element(By.XPATH, f"//*[@id='productTable']/tbody/tr{[row]}/td[2]")
-    s = driver.find_element(By.XPATH,f"//*[@id='productTable']/tbody/tr{[row]}/td[3]")
-    Author.append(A.text)
-    subject.append(s.text)
-    if s.text == "$5.99" or s.text == "$8.99":
-        p = driver.find_element(By.XPATH,f"//*[@id='productTable']/tbody/tr{[row]}/td[4]/input")
-        p.click()
-        time.sleep(2)
-print(Author)
+SAVING THE SCREENSHOT CUREENT WORKING DIRECTORY
+# save = os.getcwd()
+# driver.get("https://testautomationpractice.blogspot.com/")
+# f1 = driver.find_element(By.XPATH, "//*[@id='multipleFilesForm']/button")
+# driver.execute_script("arguments[0].scrollIntoView(true);", f1)
+# driver.save_screenshot(save+"//PK2r.png")
 
 
 
